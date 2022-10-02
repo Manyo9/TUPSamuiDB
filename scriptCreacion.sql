618cf4edb2ec552cbbb78cad8498041829c34b7f
Manyo <agusmangini@hotmail.com>
2022-10-02 15:55:11 -0300

    Subo script creacion base y un par de inserts

---
+++ scriptCreacion.sql
@@ -0,0 +1,141 @@
+-- Update 01/10/2022
+CREATE DATABASE samuidb;
+USE samuidb;
+
+CREATE TABLE  `samuidb`.`Roles` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`nombre` varchar(30) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+  
+CREATE TABLE `samuidb`.`Usuarios` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`idRol` int NOT NULL,
+	`usuario` varchar(30) NOT NULL,
+	`contrasenia` varchar(32) NOT NULL,
+	`fechaAlta` datetime NOT NULL,
+	`fechaBaja` datetime,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (idRol) REFERENCES Roles(id)
+);
+  
+  
+CREATE TABLE  `samuidb`.`Empleados` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`idUsuario` int,
+	`nombre` varchar(30) NOT NULL,
+	`apellido` varchar(30) NOT NULL,
+	`telefono` varchar(20),
+	`email` varchar(40),
+	`dni` int,
+	`fechaAlta` datetime NOT NULL,
+	`fechaBaja` datetime,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (idUsuario) REFERENCES Usuarios(id)
+); 
+  
+CREATE TABLE `samuidb`.`PuntosVenta`(
+	`id` int NOT NULL AUTO_INCREMENT,
+    `nombre` varchar(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE `samuidb`.`Socios`(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`idUsuario` int,
+	`nombre` varchar(30) NOT NULL,
+	`apellido` varchar(30) NOT NULL,
+	`domicilio` varchar(50),
+	`email` varchar(40) NOT NULL,
+	`dni` int NOT NULL,
+	`telefono` varchar(20),
+	`fechaAlta` datetime NOT NULL,
+	`fechaBaja` datetime,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (idUsuario) REFERENCES Usuarios(id));
+  
+  
+CREATE TABLE `samuidb`.`EstadosPedido`(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`nombre` varchar(32) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+
+CREATE TABLE `samuidb`.`Pedidos`(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`idPuntoVenta` int NOT NULL,
+	`idSocio` int,
+	`idEmpleado` int,
+	`idEstado` int NOT NULL,
+	`observaciones` varchar(100),
+	`fechaPedido` datetime NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (idPuntoVenta) REFERENCES PuntosVenta(id),
+	FOREIGN KEY (idSocio) REFERENCES Socios(id),
+	FOREIGN KEY (idEmpleado) REFERENCES Empleados(id)
+);
+  
+  
+CREATE TABLE `samuidb`.`Productos` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`nombre` varchar(32) NOT NULL,
+	`precio` double NOT NULL,
+	`descripcion` varchar(100), 
+	`observaciones` varchar(100), 
+	`activo` boolean NOT NULL, 
+	`puntosGanados` int NOT NULL,
+	`urlImagen` varchar(100),
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `samuidb`.`Promociones`(
+    `id` int NOT NULL AUTO_INCREMENT,
+    `nombre` varchar(50),
+    `descripcion` varchar(100),
+    `porcentajeDescuento` tinyint NOT NULL,
+    `fechaDesde` datetime NOT NULL,
+    `fechaHasta` datetime NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE `samuidb`.`PromocionesXProducto`(
+    `idProducto` int NOT NULL,
+    `idPromocion` int NOT NULL,
+    PRIMARY KEY (idProducto, idPromocion),
+    FOREIGN KEY (idProducto) REFERENCES Productos(id),
+    FOREIGN KEY (idPromocion) REFERENCES Promociones(id)
+);
+
+CREATE TABLE `samuidb`.`DetallesPedido` (
+	`idDetalle` int NOT NULL AUTO_INCREMENT,
+	`idPedido` int NOT NULL,
+	`idProducto` int NOT NULL,
+	`idPromocion` int, 
+	`cantidad` tinyint NOT NULL, 
+	`precioUnitario` double NOT NULL, 
+	`porcentajeDescuento` tinyint NOT NULL,
+	`puntosGanados` int NOT NULL,
+	`comentarios` varchar(150),
+	PRIMARY KEY (`idDetalle`) ,
+	FOREIGN KEY (`idPedido`) REFERENCES pedidos(`id`),
+	FOREIGN KEY (`idProducto`) REFERENCES productos(`id`),
+	FOREIGN KEY (`idPromocion`) REFERENCES promociones(`id`)
+);
+
+CREATE TABLE `samuidb`.`MovimientosPuntos` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`idDetalle` int NOT NULL,
+	`idSocio` int NOT NULL,
+	`puntos` int NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (idDetalle) REFERENCES DetallesPedido(`idDetalle`),
+	FOREIGN KEY (idSocio) REFERENCES Socios(`id`)
+);
+  
+ CREATE TABLE `samuidb`.`Gustos`(
+    `id` int NOT NULL AUTO_INCREMENT,
+    `nombre` varchar(30),
+    `activo` boolean NOT NULL,
+    PRIMARY KEY (`id`)
+); 
