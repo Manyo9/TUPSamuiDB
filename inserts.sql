1384ec8afe1779c8a80e9b5c82e5c55617672eb5
PMonticoli <113491@tecnicatura.frc.utn.edu.ar>
2022-10-02 16:17:51 -0300

    add user db

--- inserts.sql
+++ inserts.sql
@@ -3,3 +3,4 @@ INSERT INTO samuidb.roles (nombre) values ('Admin');
 
 INSERT INTO samuidb.usuarios (idRol, usuario, contrasenia, fechaAlta) values (1, "manyouser", "manyo123", NOW());
 INSERT INTO samuidb.usuarios (idRol, usuario, contrasenia, fechaAlta) values (2, "manyoadmin", "manyo456", NOW());
+INSERT INTO samuidb.usuarios (idRol, usuario, contrasenia, fechaAlta) values (2, "pmonticoli", "qweasd", NOW());
