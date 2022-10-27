6c666e9cf4dccb249cd0953a7ce37de4feea9d53
2w1-113491-Monticoli Pablo <113491@tecnicatura.frc.utn.edu.ar>
2022-10-27 16:17:17 -0300

    Agrego insert puntosventa y estadopedido

--- inserts.sql
+++ inserts.sql
@@ -4,3 +4,6 @@ INSERT INTO samuidb.roles (nombre) values ('Admin');
 INSERT INTO samuidb.usuarios (idRol, usuario, contrasenia, fechaAlta) values (1, "manyouser", "manyo123", NOW());
 INSERT INTO samuidb.usuarios (idRol, usuario, contrasenia, fechaAlta) values (2, "manyoadmin", "manyo456", NOW());
 INSERT INTO samuidb.usuarios (idRol, usuario, contrasenia, fechaAlta) values (1, "pmonticoli", "qweasd123", NOW());
+
+INSERT INTO samuidb.puntosventa (nombre) values ('Sucursal Samui');
+INSERT INTO samuidb.estadospedido(nombre) values('pendiente');
