afcb47bbf216e74d42df2dde39324ba3007dfe3f
Abraguas <fmoises2002@gmail.com>
2022-10-27 16:40:11 -0300

    Agrego sp para CRUD de productos

--- SPs.sql
+++ SPs.sql
@@ -27,4 +27,67 @@ IN contrasenia varchar(32)
 BEGIN 
 INSERT INTO usuarios(idRol, usuario, contrasenia, fechaAlta) values (2, usuario, contrasenia, NOW());
 END//
+
+CREATE PROCEDURE spObtenerProductos()
+BEGIN
+    SELECT id, nombre, precio, descripcion, observaciones, activo, puntosGanados, urlImagen
+	FROM productos;
+END //
+
+CREATE PROCEDURE spObtenerProductoPorID(
+	IN id int
+)
+BEGIN
+    SELECT p.id, p.nombre, p.precio, p.descripcion, p.observaciones, p.activo, p.puntosGanados, p.urlImagen
+	FROM productos p
+    WHERE p.id = id;
+END //
+
+CREATE PROCEDURE spInsertarProducto(
+IN nom varchar(32),
+IN pre double,
+IN des varchar(100),
+IN obs varchar(100),
+IN act tinyint(1),
+IN pg int,
+IN url varchar(100)
+)
+BEGIN
+    INSERT INTO productos (nombre, precio, descripcion, observaciones, activo, puntosGanados, urlImagen)
+    VALUES (nom,pre,des,obs,act,pg,url);
+END //
+
+CREATE PROCEDURE spActualizarProducto(
+IN i int,
+IN nom varchar(32),
+IN pre double,
+IN des varchar(100),
+IN obs varchar(100),
+IN act tinyint(1),
+IN pg int,
+IN url varchar(100)
+)
+BEGIN
+    UPDATE productos 
+    SET nombre = nom,
+	precio = pre,
+    descripcion = des,
+    observaciones = obs,
+    activo = act,
+    puntosGanados = pg,
+    urlImagen = url
+    WHERE id = i;
+    
+END //
+
+CREATE PROCEDURE spBorrarProducto(
+IN id int
+)
+BEGIN
+	DELETE 
+    FROM productos p
+    WHERE p.id = id;
+END //
+
+
 DELIMITER ;
