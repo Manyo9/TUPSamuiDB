e5cf82f817c9043e8666560091d22acf74424659
Manyo <agusmangini@hotmail.com>
2022-10-27 22:16:35 -0300

    updateo SPs segun refactor

--- SPs.sql
+++ SPs.sql
@@ -1,3 +1,5 @@
+-- Gestion de usuarios
+
 DELIMITER //
 CREATE PROCEDURE spIniciarSesion(
 IN usuario varchar(30),
@@ -19,7 +21,6 @@ BEGIN
 INSERT INTO usuarios(idRol, usuario, contrasenia, fechaAlta) values (1, usuario, contrasenia, NOW());
 END//
 
-
 CREATE PROCEDURE spNuevoUsuarioAdmin(
 IN usuario varchar(30),
 IN contrasenia varchar(32)
@@ -28,12 +29,16 @@ BEGIN
 INSERT INTO usuarios(idRol, usuario, contrasenia, fechaAlta) values (2, usuario, contrasenia, NOW());
 END//
 
+-- Productos
+
+-- GET ALL
 CREATE PROCEDURE spObtenerProductos()
 BEGIN
     SELECT id, nombre, precio, descripcion, observaciones, activo, puntosGanados, urlImagen
 	FROM productos;
 END //
 
+-- GET MY ID
 CREATE PROCEDURE spObtenerProductoPorID(
 	IN id int
 )
@@ -43,12 +48,13 @@ BEGIN
     WHERE p.id = id;
 END //
 
+-- CREATE
 CREATE PROCEDURE spInsertarProducto(
 IN nom varchar(32),
 IN pre double,
 IN des varchar(100),
 IN obs varchar(100),
-IN act tinyint(1),
+IN act boolean,
 IN pg int,
 IN url varchar(100)
 )
@@ -57,13 +63,14 @@ BEGIN
     VALUES (nom,pre,des,obs,act,pg,url);
 END //
 
+-- UPDATE
 CREATE PROCEDURE spActualizarProducto(
 IN i int,
 IN nom varchar(32),
 IN pre double,
 IN des varchar(100),
 IN obs varchar(100),
-IN act tinyint(1),
+IN act boolean,
 IN pg int,
 IN url varchar(100)
 )
@@ -80,6 +87,7 @@ BEGIN
     
 END //
 
+-- DELETE
 CREATE PROCEDURE spBorrarProducto(
 IN id int
 )
@@ -89,6 +97,8 @@ BEGIN
     WHERE p.id = id;
 END //
 
+-- Gestion de pedidos
+-- NEW
 CREATE PROCEDURE spRegistrarPedido(
 IN idPuntoVenta int,
 IN idSocio int,
@@ -100,20 +110,25 @@ BEGIN
     VALUES (idPuntoVenta, idSocio, idEmpleado, 1, observaciones, now());
 END //
 
+-- NEW DETALLE
 CREATE PROCEDURE spRegistrarDetallePedido(
 IN idPedido int, 
 IN idProducto int,
-IN idPromocion int,
 IN cantidad tinyint,
 IN precioUnitario double,
-IN porcentajeDescuento tinyint,
 IN puntosGanados int,
 IN comentarios varchar(150)
 )
 BEGIN
-    INSERT INTO detallespedido (idPedido, idProducto, idPromocion, cantidad, precioUnitario, porcentajeDescuento, puntosGanados, comentarios)
-    Values (idPedido, idProducto, idPromocion, cantidad, precioUnitario, porcentajeDescuento, puntosGanados, comentarios);
+    INSERT INTO detallespedido (idPedido, idProducto, cantidad, precioUnitario, puntosGanados, comentarios)
+    Values (idPedido, idProducto, cantidad, precioUnitario, puntosGanados, comentarios);
 END //
 
+-- GET all Pedidos
+CREATE PROCEDURE spObtenerPedidos()
+BEGIN
+    select p.id, p.idPuntoventa, p.idSocio, p.idEmpleado, p.idEstado, p.observaciones, p.fechaPedido
+    from Pedidos p;
+END //
 
 DELIMITER ;
