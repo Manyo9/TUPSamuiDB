57de7dd76ea953bb8b7df37db3ccbc756a4e6fd6
Manyo <agusmangini@hotmail.com>
2022-10-27 22:16:44 -0300

    Agrego insert socios

--- inserts.sql
+++ inserts.sql
@@ -6,4 +6,10 @@ INSERT INTO samuidb.usuarios (idRol, usuario, contrasenia, fechaAlta) values (2,
 INSERT INTO samuidb.usuarios (idRol, usuario, contrasenia, fechaAlta) values (1, "pmonticoli", "qweasd123", NOW());
 
 INSERT INTO samuidb.puntosventa (nombre) values ('Sucursal Samui');
+INSERT INTO samuidb.puntosventa (nombre) values ('Web');
 INSERT INTO samuidb.estadospedido(nombre) values('pendiente');
+
+INSERT INTO samuidb.socios (idUsuario,nombre,apellido,domicilio,email,dni,telefono,fechaAlta,fechaBaja) 
+values (1,'agustin','manyito','messi street 123','manyo@hotmail.com','41236598','35136544782',NOW(),NULL);
+INSERT INTO samuidb.socios (idUsuario,nombre,apellido,domicilio,email,dni,telefono,fechaAlta,fechaBaja) 
+values (3,'pablo','monticoli','ovidio lagos','pablo@hotmail.com','4214122','3525555444',NOW(),NULL);
